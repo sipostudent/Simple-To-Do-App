03a666cc4221e0e2d1a939bec8c7c9f8e77f2bcf
Sipo Charles <sipocharles@yahoo.co.uk>
2019-10-14 15:11:31 +0100

    connected Node App to database

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 let express = require('express')
+let mongodb = require('mongodb')
 
 let app = express()
+let db
+
+let connectionString = 'mongodb+srv://root:r00tUser@cluster0-0oagu.gcp.mongodb.net/TodoApp?retryWrites=true&w=majority'
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+	db = client.db()
+	app.listen(3000)
+})
 
 app.use(express.urlencoded({extended: false}))
 
@@ -57,7 +65,7 @@ app.get('/', function(req, res){
 })
 
 app.post('/create-item', function(req, res){
-    console.log(req.body.item)
+	db.collection('items').insertOne({text: req.body.item}, function(){
+		res.send("Thanks for submitting the form.")
+	})
 })
-
-app.listen(3000)
